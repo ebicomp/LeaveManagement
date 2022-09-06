5874476242378fc29fe5cc7097eee4c0982b6228
Abolfazl Karimi <ab.karimi@cbi.ir>
2022-09-06 17:58:38 +0430

    add exceptions

---
+++ HR.LeaveManagement.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Exceptions
+{
+    public class BadRequestException
+    {
+    }
+}
